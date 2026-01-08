2d18f6b547bbf21078d4920ac606b3fc0ce31d6c
hungdx-t1 <nguyenlevinhhungnt153@gmail.com>
2026-01-08 12:53:20 +0700

    refactor exception classes

--- src/main/java/com/dianxin/core/api/exceptions/lifecycle/ServiceUnavailableException.java
+++ src/main/java/com/dianxin/core/api/exceptions/lifecycle/ServiceUnavailableException.java
@@ -1,7 +1,7 @@
 package com.dianxin.core.api.exceptions.lifecycle;
 
 @SuppressWarnings("unused")
-public class ServiceUnavailableException extends IllegalStateException {
+public class ServiceUnavailableException extends UnsupportedOperationException {
     public ServiceUnavailableException(String message) {
         super(message);
     }
