2b8ea4751107db1fd2940bf7ec2ce797653ac4c5
Mino (Hung) <hungpikatv@gmail.com>
2026-01-30 12:28:07 +0700

    added json command builder (for testing)

---
+++ src/main/java/com/dianxin/core/jdaexpansion/commands/CommandBuilder.java
@@ -0,0 +1,48 @@
+package com.dianxin.core.jdaexpansion.commands;
+
+import net.dv8tion.jda.api.interactions.commands.OptionType;
+import net.dv8tion.jda.api.interactions.commands.build.CommandData;
+import net.dv8tion.jda.api.interactions.commands.build.Commands;
+import net.dv8tion.jda.api.interactions.commands.build.OptionData;
+import org.jetbrains.annotations.ApiStatus;
+import org.jetbrains.annotations.Nullable;
+import org.jspecify.annotations.NullMarked;
+
+import java.io.File;
+
+@NullMarked
+@SuppressWarnings("unused")
+@ApiStatus.AvailableSince("2.0")
+public class CommandBuilder {
+    public static CommandData of(File file) {
+        return null;
+    }
+
+    public static class CommandDataBuilder {
+        public static CommandData of(String commandName, @Nullable String description) {
+            return Commands.slash(commandName, description == null ? "No description provided" : description);
+        }
+    }
+
+    public static class OptionDataBuilder {
+        public static OptionType typeOf(String optType) throws IllegalArgumentException {
+            return switch (optType.toLowerCase()) {
+                case "int", "integer" -> OptionType.INTEGER;
+                case "string" -> OptionType.STRING;
+                case "boolean" -> OptionType.BOOLEAN;
+                case "user" -> OptionType.USER;
+                case "channel"  -> OptionType.CHANNEL;
+                case "role" -> OptionType.ROLE;
+                case "mentionable" -> OptionType.MENTIONABLE;
+                case "number" ->  OptionType.NUMBER;
+                case "attachment" -> OptionType.ATTACHMENT;
+                default -> throw new IllegalArgumentException("Unknown option type: " + optType);
+            };
+        }
+
+        public static OptionData of(String optType, String optionName, @Nullable String description) throws IllegalArgumentException {
+            OptionType type = CommandBuilder.OptionDataBuilder.typeOf(optType);
+            return new OptionData(type, optionName, description == null ? "No description provided" : description);
+        }
+    }
+}
