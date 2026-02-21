ba12c3327a8417ea800a6ae6a538df7496e21c55
Mino (Hung) <hungpikatv@gmail.com>
2026-02-21 20:57:12 +0700

    added instance (cont.d)

---
+++ src/main/java/com/dianxin/core/jda/handler/console/example/OldStopConsoleCommand.java
@@ -0,0 +1,20 @@
+package com.dianxin.core.jda.handler.console.example;
+
+import com.dianxin.core.jda.JavaDiscordBot;
+import com.dianxin.core.jda.handler.console.AbstractConsoleCommand;
+
+@SuppressWarnings("unused")
+public class OldStopConsoleCommand extends AbstractConsoleCommand {
+    private final JavaDiscordBot bot; // hoặc bot con
+
+    public OldStopConsoleCommand(JavaDiscordBot bot) {
+        super("stop");
+        this.bot = bot;
+    }
+
+    @Override
+    public void execute(String[] args) {
+        bot.onShutdown();
+        System.exit(0);
+    }
+}
